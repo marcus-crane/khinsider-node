b88a63da2f2867eec25ac83692aa4c7fa67602d5
Marcus <marcus@thingsima.de>
2017-07-02 01:55:20 +1200

    Rubbish code but now parses discogs too

--- khinsider.js
+++ khinsider.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const prog = require('caporal')
+require('dotenv').config()
 const getTrackList = require('./lib/getTrackList')
 
 prog
@@ -7,8 +8,9 @@ prog
 
   .command('download', 'Download an OST from KHInsider')
   .argument('<link>', 'The segment after /album/ in the OST URL')
+  .option('--id <id>', 'Fetch track names from Discogs using album ID')
   .action((args, options) => {
-    getTrackList(args.link)
+    getTrackList(args.link, options.id)
   })
 
 prog.parse(process.argv)
