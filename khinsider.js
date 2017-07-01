cdf1851aeeead41665921054b3c946d85ef65dab
Marcus <marcus@thingsima.de>
2017-07-02 03:38:58 +1200

    Added ID3 tagging

--- khinsider.js
+++ khinsider.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const prog = require('caporal')
-require('dotenv').config()
 const getTrackList = require('./lib/getTrackList')
+const writeTags = require('./lib/writeTags')
 
 prog
   .version('1.0.0')
@@ -11,6 +11,12 @@ prog
   .option('--id <id>', 'Fetch track names from Discogs using album ID')
   .action((args, options) => {
     getTrackList(args.link, options.id)
+  })
+
+  .command('tag', 'Generate ID3 tags from Discogs')
+  .argument('<id>', 'Discogs ID of the downloaded album')
+  .action((args, options) => {
+    writeTags(args.id)
   })
 
 prog.parse(process.argv)
